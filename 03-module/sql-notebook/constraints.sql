cd76207a3c389b42c7bb07f8937d7de77db58dce
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-07-18 05:24:08 +0530

    constraint: forign key

--- 03-module/sql-notebook/constraints.sql
+++ 03-module/sql-notebook/constraints.sql
@@ -8,3 +8,13 @@ create table employees (
     emp_nm varchar(50),
     age int
 );
+
+-- Foreign Key Constraint
+
+create table orders(
+    ord_id int primary key,
+    emp_id int,
+    ord_dt date,
+    foreign key (emp_id) references employees (emp_id)
+);
+
